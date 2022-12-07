e9995ab645527b94388b4aaf684857314ace5c17
RomnShnitser <117955089+RomnShnitser@users.noreply.github.com>
2022-12-08 01:07:28 +0200

    From_w10

---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,46 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	WebDriver wd;
+	
+	@BeforeMethod
+	public void init() {
+		wd = new ChromeDriver();
+		wd.navigate().to("https://contacts-app.tobbymarshall815.vercel.app/home");
+	}
+	
+	@Test
+	public void loginPositiveTest() {
+		// open login/reg form
+		WebElement loginBtn = wd.findElement(By.xpath("//a[text()='LOGIN']"));
+		loginBtn.click();
+		
+		// fill login/reg form
+		WebElement emailInput = wd.findElement(By.xpath("//input[1]"));
+		emailInput.click();
+		emailInput.clear();
+		emailInput.sendKeys("roman@googooloo.com");
+		
+		WebElement passInput = wd.findElement(By.xpath("//input[2]"));
+		passInput.click();
+		passInput.clear();
+		passInput.sendKeys("Abcd1234");
+		
+		
+		// click "login" button
+		wd.findElement(By.xpath("//button[1]")).click();
+		// assert
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+		//wd.quit();
+	}
+}
